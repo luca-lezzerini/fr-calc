f62fb1c1db23a2225361202be83c139c1248e646
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-12-30 20:29:08 +0100

    Prima versione

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  risultato: number = 0;
+  add1: number = 0;
+  add2: number = 0;
+}

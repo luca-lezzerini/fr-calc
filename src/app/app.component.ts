ef73fd5104565d2c6cd3fdca6d0aac3d6fe47206
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-12-30 21:26:28 +0100

    Lista della spesa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,20 @@ export class AppComponent {
   risultato: number = 0;
   add1: number = 0;
   add2: number = 0;
+  cosa: string = "";
+  lista: string[] = [];
 
   calcola() {
     this.risultato = this.add1 + this.add2;
+  }
+
+  aggiungiAllaLista() {
+    // aggiunge in fondo all'array un nuovo elemento
+    this.lista.push(this.cosa);
+    console.log(this.lista);
+  }
+
+  cancella(inx: number) {
+    this.lista.splice(inx, 1);
   }
 }
